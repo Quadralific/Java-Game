0cf4e34d8e51367937108c1ac619b0338b607bfa
Tom L <devzemux1613@gmail.com>
2023-08-06 22:53:35 +0200

    fix: frame borders and ball spawning

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,5 @@ public class Main {
     public static void main(String[] args)  {
         new Window("Game");
     }
-    
-    
-    
+
 }
--- src/window/GamePanel.java
+++ src/window/GamePanel.java
@@ -11,12 +11,13 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public final class GamePanel extends JPanel implements Runnable {
-    private final int WINDOW_SIZE = 700;
+
+    public static final int WINDOW_SIZE = 700;
+    private final Player player;
+    private final Orb orb;
     private int score = 0;
     private boolean running = false;
     private Thread thread;
-    private Player player;
-    private Orb orb;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WINDOW_SIZE, WINDOW_SIZE));
@@ -26,7 +27,7 @@ public final class GamePanel extends JPanel implements Runnable {
         addKeyListener(new KeyProcessor());
 
         player = new Player(250, 250);
-        orb = new Orb(RandomNumbers.generate(1000), RandomNumbers.generate(1000));
+        orb = new Orb(RandomNumbers.generate(WINDOW_SIZE), RandomNumbers.generate(WINDOW_SIZE));
         start();
     }
 
@@ -38,7 +39,9 @@ public final class GamePanel extends JPanel implements Runnable {
 
     @Override
     protected void paintComponent(Graphics g) {
-        if(!running) return;
+        if (!running) {
+            return;
+        }
 
         super.paintComponent(g);
         player.init(g);
@@ -46,37 +49,54 @@ public final class GamePanel extends JPanel implements Runnable {
 
         g.setColor(Color.WHITE);
         g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
-        g.drawString("Score: " + score, 250, 20);
+        g.drawString("Score: " + score, (WINDOW_SIZE / 2) - 50, 20);
         g.dispose();
     }
 
     private void update() {
-        final Rectangle rect = new Rectangle(player.x, player.y, 25, 25),
-                        rec2 = new Rectangle(orb.x, orb.y, 25, 25);
-        if(rect.intersects(rec2)) {
-            orb.x = RandomNumbers.generate(800);
-            orb.y = RandomNumbers.generate(800);
+        final Rectangle rect = new Rectangle(player.x, player.y, 25, 25), rec2 = new Rectangle(orb.x, orb.y, 25, 25);
+        if (rect.intersects(rec2)) {
+            orb.x = RandomNumbers.generate(WINDOW_SIZE - 25);
+            orb.y = RandomNumbers.generate(WINDOW_SIZE - 25);
             score++;
         }
     }
 
     @Override
     public void run() {
-        while(running) {
+        while (running) {
             update();
             repaint();
         }
     }
 
     private final class KeyProcessor extends KeyAdapter {
+
         @Override
         public void keyPressed(KeyEvent e) {
             super.keyPressed(e);
-            switch(e.getKeyCode()) {
-                case KeyEvent.VK_W -> player.y -= 25;
-                case KeyEvent.VK_A -> player.x -= 25;
-                case KeyEvent.VK_S -> player.y += 25;
-                case KeyEvent.VK_D -> player.x += 25;
+            final int stepPixel = 25;
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_W -> {
+                    if (player.y - stepPixel >= 0) {
+                        player.y -= stepPixel;
+                    }
+                }
+                case KeyEvent.VK_A -> {
+                    if (player.x - stepPixel >= 0) {
+                        player.x -= stepPixel;
+                    }
+                }
+                case KeyEvent.VK_S -> {
+                    if (player.y + stepPixel <= WINDOW_SIZE - stepPixel) {
+                        player.y += stepPixel;
+                    }
+                }
+                case KeyEvent.VK_D -> {
+                    if (player.x + stepPixel <= WINDOW_SIZE - stepPixel) {
+                        player.x += stepPixel;
+                    }
+                }
             }
         }
     }
